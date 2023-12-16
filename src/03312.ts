f13d9d8fd5f963184b82b8fb3d6f988ce10a461c
toona <amanou.toona@gmail.com>
2023-12-17 01:13:42 +0900

    03312: AC

---
+++ src/03312.ts
@@ -0,0 +1,8 @@
+// MyParametersを実装してください
+// MyParametersは関数の引数の型をタプルで返す型です
+//
+type MyParameters<T extends (...args: any) => any> = T extends (...args: infer R) => any
+    ? R : never;
+
+const foo = (arg1: string, arg2: number): void => { };
+type FunctionParamsType = MyParameters<typeof foo>; // [arg1: string, arg2: number]
