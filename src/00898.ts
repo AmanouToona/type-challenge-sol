de084b02f63e9d1a3a3d38be06546c1b88f780e2
toona <amanou.toona@gmail.com>
2023-12-17 21:52:37 +0900

    00898: AC

--- src/00898.ts
+++ src/00898.ts
@@ -1,5 +1,5 @@
 // Includesを実装してください
 
-type Includes<T extends any[], U> = keyof T extends U ? true : false;
+type Includes<T extends any[], U> = U extends T[number] ? true : false;
 
 type isPillarMen = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'> // expected to be `false`
