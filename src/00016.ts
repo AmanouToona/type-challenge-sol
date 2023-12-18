db7b1e7c02b8eab1710afd52d57f9661d9205eda
toona <amanou.toona@gmail.com>
2023-12-18 21:32:26 +0900

    00016: AC

---
+++ src/00016.ts
@@ -0,0 +1,9 @@
+// Pop型を実装してください。
+// Pop型は、配列の最後の要素を削除した配列を返す型です。
+type arr1 = ['a', 'b', 'c', 'd']
+type arr2 = [3, 2, 1]
+
+type Pop<T extends any[]> = T extends [...infer R, any] ? R : never;
+
+type re1 = Pop<arr1> // expected to be ['a', 'b', 'c']
+type re2 = Pop<arr2> // expected to be [3, 2]
