a8ac1ccd98fab2662e36cc188a3d1e162af58d15
toona <amanou.toona@gmail.com>
2023-12-25 22:03:56 +0900

    00599: AC

--- 00599.ts
+++ 00599.ts
@@ -10,6 +10,5 @@ type coo = {
     sex: string;
 };
 
-type Merge<T, U> = { [Property in keyof (T | U)]: Property extends keyof T ? T[Property] : U[Property] };
-
+type Merge<T, U> = { [Property in keyof T | keyof U]: Property extends keyof U ? U[Property] : Property extends keyof T ? T[Property] : never };
 type Result = Merge<foo, coo>; // expected to be {name: string, age: number, sex: string}
