2614b68ca56eae2f6a79a408911dfffd81f29021
toona-laptop <amanou.toona@gmail.com>
2024-01-02 16:37:38 +0900

    00108: AC

---
+++ src/00108.ts
@@ -0,0 +1,4 @@
+// 文字列から両端の空白を削除した新しい文字列を返すTrim<T>を実装してください。
+
+type Trim<T extends string> = T extends `${' '}${infer R}` | `${infer R}${' '} ` ? Trim<R> : T;
+type trimed = Trim<"  Hello World  ">; // expected to be 'Hello World'
